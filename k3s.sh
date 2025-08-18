0b11702ec9df39759a601f19d732a304a78d64cc
imasavior <61541124+imasavior@users.noreply.github.com>
2025-08-18 15:44:18 +0800

    Update k3s.sh

--- k3s.sh
+++ k3s.sh
@@ -55,6 +55,8 @@ echo -e "${CYAN}[*] Downloading Flannel YAML...${NC}"
 wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
 sudo apt install -y ansible
+sudo apt install -y tree
+
 echo -e "${BLUE}[*] Waiting 60 seconds for ECK components to start...${NC}"
 sleep 60
 kubectl get po -A
